5b5e7f0c2776aba80f9d67672526476682f32d48
phajy <andy.young@bristol.ac.uk>
2024-06-20 16:01:03 +0100

    feat: include first NuSTAR data set in plot with XMM data

--- src/quick-look.jl
+++ src/quick-look.jl
@@ -6,7 +6,7 @@ using Plots
 data_directory = "data"
 cd(data_directory)
 
-# load and prepare obsid 0693781201
+# load and prepare XMM obsid 0693781201
 xmm_data_a = XmmData(
     XmmEPIC(),
     "PN_spectrum_grp_0693781201_S003_total.fits",
@@ -21,7 +21,7 @@ mask_energies!(xmm_data_a, 3.0, 10.0)
 subtract_background!(xmm_data_a)
 normalize!(xmm_data_a)
 
-# load and prepare obsid 0693781301
+# load and prepare XMM obsid 0693781301
 xmm_data_b = XmmData(
     XmmEPIC(),
     "PN_spectrum_grp_0693781301_S003_total.fits",
@@ -36,7 +36,7 @@ mask_energies!(xmm_data_b, 3.0, 10.0)
 subtract_background!(xmm_data_b)
 normalize!(xmm_data_b)
 
-# load and prepare obsid 0693781401
+# load and prepare XMM obsid 0693781401
 xmm_data_c = XmmData(
     XmmEPIC(),
     "PN_spectrum_grp_0693781401_S003_total.fits",
@@ -51,14 +51,37 @@ mask_energies!(xmm_data_c, 3.0, 10.0)
 subtract_background!(xmm_data_c)
 normalize!(xmm_data_c)
 
+# load and prepare NuSTAR obsid 60001047002
+nustar_a_fpma = NuStarData(
+    "nu60001047002A01_sr_min20.pha"
+)
+
+regroup!(nustar_a_fpma)
+drop_bad_channels!(nustar_a_fpma)
+mask_energies!(nustar_a_fpma, 3.0, 50.0)
+subtract_background!(nustar_a_fpma)
+normalize!(nustar_a_fpma)
+
+nustar_a_fpmb = NuStarData(
+    "nu60001047002B01_sr_min20.pha"
+)
+
+regroup!(nustar_a_fpmb)
+drop_bad_channels!(nustar_a_fpmb)
+mask_energies!(nustar_a_fpmb, 3.0, 50.0)
+subtract_background!(nustar_a_fpmb)
+normalize!(nustar_a_fpmb)
+
 model = PowerLaw()
 prob = FittingProblem(
-    FittableMultiModel(model, model, model),
-    FittableMultiDataset(xmm_data_a, xmm_data_b, xmm_data_c)
+    FittableMultiModel(model, model, model, model, model),
+    FittableMultiDataset(xmm_data_a, xmm_data_b, xmm_data_c, nustar_a_fpma, nustar_a_fpmb)
 )
 
 result = fit(prob, LevenbergMarquadt())
 
-plotresult(xmm_data_a, result[1], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(0.05, 2))
-plotresult!(xmm_data_b, result[2], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(0.05, 2))
-plotresult!(xmm_data_c, result[3], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(0.05, 2))
+plotresult(xmm_data_a, result[1], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
+plotresult!(xmm_data_b, result[2], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
+plotresult!(xmm_data_c, result[3], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
+plotresult!(nustar_a_fpma, result[4], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
+plotresult!(nustar_a_fpmb, result[5], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
