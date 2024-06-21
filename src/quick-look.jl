1c4da32b004655f527e03f098eebc25ed627a994
phajy <andy.young@bristol.ac.uk>
2024-06-21 09:52:45 +0100

    feat: streamline reading of XMM and NuSTAR data

--- src/quick-look.jl
+++ src/quick-look.jl
@@ -3,85 +3,67 @@
 using SpectralFitting
 using Plots
 
-data_directory = "data"
-cd(data_directory)
+data_path = "data"
 
-# load and prepare XMM obsid 0693781201
-xmm_data_a = XmmData(
-    XmmEPIC(),
-    "PN_spectrum_grp_0693781201_S003_total.fits",
-    response="PN_0693781201_S003_total.rmf",
-    ancillary="PN_0693781201_S003_total.arf",
-    background="PNbackground_spectrum_0693781201_S003_total.fits"
-)
+# XMM data
 
-regroup!(xmm_data_a)
-drop_bad_channels!(xmm_data_a)
-mask_energies!(xmm_data_a, 3.0, 10.0)
-subtract_background!(xmm_data_a)
-normalize!(xmm_data_a)
+function prepare_xmm(spec, back, rmf, arf)
+    ds = XmmData(
+        XmmEPIC(),
+        spec,
+        background=back,
+        response=rmf,
+        ancillary=arf
+    )
+    regroup!(ds)
+    drop_bad_channels!(ds)
+    mask_energies!(ds, 3.0, 10.0)
+    subtract_background!(ds)
+    normalize!(ds)
+    return ds
+end
 
-# load and prepare XMM obsid 0693781301
-xmm_data_b = XmmData(
-    XmmEPIC(),
-    "PN_spectrum_grp_0693781301_S003_total.fits",
-    response="PN_0693781301_S003_total.rmf",
-    ancillary="PN_0693781301_S003_total.arf",
-    background="PNbackground_spectrum_0693781301_S003_total.fits"
-)
+xmm_data = []
+for i in 2:4
+    spec = joinpath(data_path, "PN_spectrum_grp_0693781$(i)01_S003_total.fits")
+    back = joinpath(data_path, "PNbackground_spectrum_0693781$(i)01_S003_total.fits")
+    rmf = joinpath(data_path, "PN_0693781$(i)01_S003_total.rmf")
+    arf = joinpath(data_path, "PN_0693781$(i)01_S003_total.arf")
+    push!(xmm_data, prepare_xmm(spec, back, rmf, arf))
+end
 
-regroup!(xmm_data_b)
-drop_bad_channels!(xmm_data_b)
-mask_energies!(xmm_data_b, 3.0, 10.0)
-subtract_background!(xmm_data_b)
-normalize!(xmm_data_b)
+# NuSTAR data
 
-# load and prepare XMM obsid 0693781401
-xmm_data_c = XmmData(
-    XmmEPIC(),
-    "PN_spectrum_grp_0693781401_S003_total.fits",
-    response="PN_0693781401_S003_total.rmf",
-    ancillary="PN_0693781401_S003_total.arf",
-    background="PNbackground_spectrum_0693781401_S003_total.fits"
-)
+function prepare_nustar(data_path, obsid, fpm)
+    ds = NuStarData(joinpath(data_path, "nu$(obsid)$(fpm)01_sr_min20.pha"))
+    regroup!(ds)
+    drop_bad_channels!(ds)
+    mask_energies!(ds, 3.0, 50.0)
+    subtract_background!(ds)
+    normalize!(ds)
+    return ds
+end
 
-regroup!(xmm_data_c)
-drop_bad_channels!(xmm_data_c)
-mask_energies!(xmm_data_c, 3.0, 10.0)
-subtract_background!(xmm_data_c)
-normalize!(xmm_data_c)
+nustar_data = []
+for obsid in ["60001047002"]
+    for fpm in ["A", "B"]
+        push!(nustar_data, prepare_nustar(data_path, obsid, fpm))
+    end
+end
 
-# load and prepare NuSTAR obsid 60001047002
-nustar_a_fpma = NuStarData(
-    "nu60001047002A01_sr_min20.pha"
-)
-
-regroup!(nustar_a_fpma)
-drop_bad_channels!(nustar_a_fpma)
-mask_energies!(nustar_a_fpma, 3.0, 50.0)
-subtract_background!(nustar_a_fpma)
-normalize!(nustar_a_fpma)
-
-nustar_a_fpmb = NuStarData(
-    "nu60001047002B01_sr_min20.pha"
-)
-
-regroup!(nustar_a_fpmb)
-drop_bad_channels!(nustar_a_fpmb)
-mask_energies!(nustar_a_fpmb, 3.0, 50.0)
-subtract_background!(nustar_a_fpmb)
-normalize!(nustar_a_fpmb)
+# Fit
 
 model = PowerLaw()
 prob = FittingProblem(
     FittableMultiModel(model, model, model, model, model),
-    FittableMultiDataset(xmm_data_a, xmm_data_b, xmm_data_c, nustar_a_fpma, nustar_a_fpmb)
+    FittableMultiDataset(xmm_data[1], xmm_data[2], xmm_data[3], nustar_data[1], nustar_data[2])
 )
-
 result = fit(prob, LevenbergMarquadt())
 
-plotresult(xmm_data_a, result[1], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
-plotresult!(xmm_data_b, result[2], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
-plotresult!(xmm_data_c, result[3], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
-plotresult!(nustar_a_fpma, result[4], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
-plotresult!(nustar_a_fpmb, result[5], xscale=:log10, yscale=:log10, xlims=(3, 50), ylims=(1e-4, 2))
+# Plot
+
+plotresult(xmm_data[1], result[1], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(5e-2, 2))
+plotresult!(xmm_data[2], result[2], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(5e-2, 2))
+plotresult!(xmm_data[3], result[3], xscale=:log10, yscale=:log10, xlims=(3, 10), ylims=(5e-2, 2))
+plotresult!(nustar_data[1], result[4], xscale=:log10, yscale=:log10, xlims=(3,50), ylims=(1e-4,2))
+plotresult!(nustar_data[2], result[5], xscale=:log10, yscale=:log10, xlims=(3,50), ylims=(1e-4,2))
